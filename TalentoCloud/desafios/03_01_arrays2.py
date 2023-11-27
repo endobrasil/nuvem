49495f2771038d362f52eeb273fc027a25aa57ff
endobrasil <endobrasil@yahoo.com.br>
2023-11-27 15:41:58 -0300

    array com indices

---
+++ TalentoCloud/desafios/03_01_arrays2.py
@@ -0,0 +1,29 @@
+"""
+	Copie o array apresentado embaixo no 
+	seu editor de código, e imprima no terminal: 
+	a quantidade de elementos que ele possui,
+	 o dado salvo no índice 2, o dado salvo no índice 9,
+	  e dado salvo no índice 14.
+
+	lista_musicos = [ 'Djavan', 'Roberto Carlos', 
+		'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 
+		'Chico Buarque', 'Nara Leão', 'Pitty', 
+		'Simonal', 'Moacir Santos', 'Caetano Veloso', 
+		'Elza Soares', 'Paulinho da Viola', 
+		'Yamandú Costa', 'Gal Costa'] 
+"""
+
+#lista base
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+#quantidade de elementos
+print(len(lista_musicos))
+
+#elemento salvo no índice 2
+print(lista_musicos[2])
+
+#elemento salvo no índice 9
+print(lista_musicos[9])
+
+#elemento salvo no índice 14
+print(lista_musicos[14])
