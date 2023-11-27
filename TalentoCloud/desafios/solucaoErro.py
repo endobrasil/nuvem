b5cd0e8c1146a23e8eb90289d6154e95ca3dabdf
endobrasil <endobrasil@yahoo.com.br>
2023-11-27 15:33:50 -0300

    desafio array

---
+++ TalentoCloud/desafios/solucaoErro.py
@@ -0,0 +1,24 @@
+#Solução:
+
+numeroCorreto = False
+
+while (numeroCorreto == False):
+
+   print("Insira um número par")
+
+   try:
+
+       numero = int(input())
+
+       if (numero%2 == 0):
+
+           numeroCorreto = True
+
+           print("Você digitou um numero par !")
+
+       else :
+
+           print("Você digitou um número impar")
+   except:
+
+       print("Caracter inválido")
