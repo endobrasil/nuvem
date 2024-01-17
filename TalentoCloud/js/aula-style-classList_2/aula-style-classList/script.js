6aa4aef7b591b2abf220789f2c6aa3a058aee020
endobrasil <endobrasil@yahoo.com.br>
2024-01-16 21:24:26 -0300

    stylos e classes com DOM

---
+++ TalentoCloud/js/aula-style-classList_2/aula-style-classList/script.js
@@ -0,0 +1,11 @@
+const elementoH2=document.querySelector('h2');
+
+elementoH2.style.color="red";
+
+console.log(elementoH2);
+let elementoLabel = document.querySelector("label");
+
+elementoLabel.style.color="yellow";
+console.log(elementoLabel);
+
+
